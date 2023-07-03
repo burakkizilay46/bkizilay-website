ea7a856913e42c59c480fc3371935a790ad07a49
Burak Kızılay <kizilay4696@gmail.com>
2023-07-03 16:03:26 +0300

    deploy error fixs

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,15 +8,6 @@ const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-HYBWE4G5Q4"
-      ></script>
-      <script>
-        window.dataLayer = window.dataLayer || []; function gtag()
-        {dataLayer.push(arguments)}
-        gtag('js', new Date()); gtag('config', 'G-HYBWE4G5Q4');
-      </script>
       <body className={inter.className}>
         <Layout>{children}</Layout>
       </body>
