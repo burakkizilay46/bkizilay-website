1b4440893f6ed683deb2071fab6e38d44ae4a8d9
Burak Kızılay <kizilay4696@gmail.com>
2023-07-03 16:12:50 +0300

    vercel analytics add

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import Header from "@/layout/components/header";
 import React from "react";
 import "../app/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function MyApp({ Component, pageProps }) {
   const Layout = Component.Layout ? Component.Layout : React.Fragment;
@@ -8,6 +9,7 @@ export default function MyApp({ Component, pageProps }) {
     <div>
       <Layout>
         <Component {...pageProps} />
+        <Analytics />
       </Layout>
     </div>
   );
